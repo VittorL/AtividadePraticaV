cd229ff3c80eae25d37f2b03051135ed18705857
Vittor Henrique <72000619@aluno.faculdadecotemig.br>
2021-09-13 20:48:51 -0300

    Finalização do Projeto
    Foi criado todas as funções requeridas na atividade

--- GestorBancario/src/Conta.java
+++ GestorBancario/src/Conta.java
@@ -3,12 +3,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Conta {
-	public int numero;
-	public double saldo = 0;
 	public int codigo;
-	List<Conta> contaLista = new ArrayList<Conta>();
+	public int numero;
+	public double saldo = 0;	
+	static List<Conta> contaLista = new ArrayList<Conta>();
 	Scanner s = new Scanner(System.in);
-
+	
+	MenuInicial menu = new MenuInicial();
+	
 	public int getNumero() {
 		return numero;
 	}
@@ -26,5 +28,88 @@ public class Conta {
 	}
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
+	}
+	
+	public void cadastrarConta() {
+		
+		System.out.println("Digite o número da conta do cliente: ");
+		this.setNumero(s.nextInt());
+				
+		contaLista.add(this);
+		menu.Menus();	
+	}
+	
+	public void depositar() {
+		System.out.println("Digite o número da conta que deseja depositar: ");
+		int idDepositar = new Scanner(System.in).nextInt();
+		 
+		 
+		for (int i = 0; i < contaLista.size(); i++) {
+			if(contaLista.get(i).numero == idDepositar) {	
+				
+				System.out.println("Digite o valor deseja depositar: ");
+				double valor = new Scanner(System.in).nextDouble();
+				
+				if (valor != -1) {
+					double saldoResult = contaLista.get(i).saldo + valor;
+					contaLista.get(i).setSaldo(saldoResult);
+				} else {
+					
+					System.out.println("Não efetuado!");				
+				}
+				
+				
+			}
+		}
+		menu.Menus();
+	}	
+	
+	public boolean sacar() {
+		System.out.println("Digite o número da conta que deseja Sacar: ");
+		int idSacar = new Scanner(System.in).nextInt();
+		 
+		 
+		for (int i = 0; i < contaLista.size(); i++) {
+			if(contaLista.get(i).numero == idSacar) {	
+				System.out.println("Digite o valor deseja Sacar: ");
+				double valor = new Scanner(System.in).nextDouble();
+				
+				if (valor > contaLista.get(i).saldo) {
+					System.out.println("Nao ha saldo suficiente");
+					menu.Menus();
+				} else {
+					double saldoResult = contaLista.get(i).saldo - valor;
+					contaLista.get(i).setSaldo(saldoResult);
+					menu.Menus();
+				}
+				
+				
+			}
+		}
+		
+		return false;
+	}
+	
+	public void verificaSaldo() {
+		System.out.println("Digite o número da conta que deseja verificar Saldo: ");
+		int idSaldo = new Scanner(System.in).nextInt();
+		for (int i = 0; i < contaLista.size(); i++) {
+			if(contaLista.get(i).numero == idSaldo) {	
+				System.out.println("verificar Saldo: "+contaLista.get(i).saldo);
+			}
+		}
+		menu.Menus();
+	}
+	
+	public static void Consulta() {
+				
+		for (Conta c: contaLista) {
+			int id = c.codigo;
+			int numConta = c.numero;
+			int saldo = (int) c.saldo;
+			System.out.println("Registro: "+id+" | Número da Conta: "+numConta+ " | Saldo: " +saldo);
+			}
+		MenuInicial men = new MenuInicial();
+		men.Menus();
 	}
 }
--- GestorBancario/src/MenuInicial.java
+++ GestorBancario/src/MenuInicial.java
@@ -11,8 +11,9 @@ public class MenuInicial {
 		System.out.println("\n                  ============================");
 		System.out.println("                  |1 - Criar Conta Corrente |");
 		System.out.println("                  |2 - Realizar Depósito    |");
-		System.out.println("                  |0 - Realizar Saque       |");
-		System.out.println("                  |0 - Consultar Saldo		|");
+		System.out.println("                  |3 - Realizar Saque       |");
+		System.out.println("                  |4 - Consultar Saldo	    |");
+		System.out.println("                  |5 - Contas Cadastradas   |");
 		System.out.println("                  |0 - Sair                 |");
 		System.out.println("                  ============================\n");
 
@@ -22,7 +23,31 @@ public class MenuInicial {
 	
 		System.out.print("\n");
 			
-		
+		switch (opcao) {
+		case 1:
+			System.out.println("Realizar Cadastro: ");
+			System.out.print("\n");
+			new Conta().cadastrarConta();
+			break;
+		case 2:
+			new Conta().depositar();				                 
+		case 3:
+			new Conta().sacar();			
+			break;
+		case 4:
+			new Conta().verificaSaldo();
+			break;
+		case 5:
+			new Conta().Consulta();
+			break;
+		case 0:
+			MenuInicial men = new MenuInicial();
+			men.Menus();		
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+			break;
+			}
 		
 	}
 }
