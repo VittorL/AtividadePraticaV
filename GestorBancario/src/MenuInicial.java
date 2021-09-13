001b88508e11dabdc32e0a0fd19ba913966b36fe
Vittor Henrique <72000619@aluno.faculdadecotemig.br>
2021-09-13 19:44:22 -0300

    Criando Menu

--- GestorBancario/src/MenuInicial.java
+++ GestorBancario/src/MenuInicial.java
@@ -1,4 +1,28 @@
-
+import java.util.Scanner;
 public class MenuInicial {
+	
+	public void Menus() {
+		
+		Scanner s = new Scanner(System.in);
+		
+	
+		System.out
+		.println("\n\nTRABALHO POO - ATIVIDADE 2");
+		System.out.println("\n                  ============================");
+		System.out.println("                  |1 - Criar Conta Corrente |");
+		System.out.println("                  |2 - Realizar Depósito    |");
+		System.out.println("                  |0 - Realizar Saque       |");
+		System.out.println("                  |0 - Consultar Saldo		|");
+		System.out.println("                  |0 - Sair                 |");
+		System.out.println("                  ============================\n");
 
+	
+		System.out.print("Opção ->");
+		int opcao = s.nextInt();
+	
+		System.out.print("\n");
+			
+		
+		
+	}
 }
